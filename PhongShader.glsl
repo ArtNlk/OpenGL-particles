fa4b4ac4a2a9ad8938510db1eb2980a04f0d93a9
ArtNlk <artemthegood@gmail.com>
2021-11-25 01:04:29 +0300

    Implemented phong shading

--- PhongShader.glsl
+++ PhongShader.glsl
@@ -11,7 +11,10 @@ uniform vec4 sunColor;
 void main(void)
 {
     //fragColor = vec4(vNorm.xyz,1.f);
+    vec3 viewDir = normalize(camPos - vPos);
+    vec3 reflectDir = reflect(-sunDir, vNorm);
     vec3 ambient = ambientLight.xyz;
     vec3 diffuse = sunColor.xyz * max(dot(vNorm, normalize(sunDir)), 0.0);
-    fragColor = vec4((ambient+diffuse)*objectColor.xyz,1.f);
+    vec3 specular = pow(max(dot(viewDir, reflectDir), 0.0), 28) * 0.5 * sunColor.xyz;
+    fragColor = vec4((ambient+diffuse+specular)*objectColor.xyz,1.f);
 }
